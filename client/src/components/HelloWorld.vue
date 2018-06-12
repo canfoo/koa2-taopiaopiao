270043a82f02548569c5d14b98a8bea6c472648f
chensenwei <chensenwei@meituan.com>
2018-06-12 21:52:13 +0800

    frame dome

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links111</h2>
+    <h2>Essential Links1</h2>
     <ul>
       <li>
         <a
--- server/main.js
+++ server/main.js
@@ -2,10 +2,12 @@ const path = require('path')
 const Koa = require('koa')
 const koaStatic = require('koa-static')
 const views = require('koa-views')
+const chalk = require('chalk')
 const routers = require('./routers')
 
 const app = new Koa()
 const port = process.env.SERVER_PORT && Number(process.env.SERVER_PORT)
+const isOnline = process.env.ONLINE
 
 app.use(koaStatic(
   path.join(__dirname , '../dist')
@@ -18,4 +20,6 @@ app.use(views(path.join(__dirname, './views'), {
 
 app.use(routers.routes()).use(routers.allowedMethods())
 
-app.listen(port)
+app.listen(port, () => {
+  isOnline && console.log(chalk.yellow(`service listening on port ${port}...`))
+})
