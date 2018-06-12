9775587b94518893e4941e7592db578e6f824e3e
chensenwei <chensenwei@meituan.com>
2018-06-12 19:57:02 +0800

    deploy done

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,9 +3,12 @@ const creator = require('../service/creator')
 module.exports = async ( ctx ) => {
  const title = 'home'
  const Arr = [{name: 1}, {name: 2}]
+ const cssList = creator.styleCreator()
  const scriptList = creator.scriptCreator()
+
   await ctx.render('index', {
     title,
+    cssList,
     scriptList
   })
 }
--- server/service/creator.js
+++ server/service/creator.js
@@ -1,11 +1,23 @@
 
 const config = require('../../config')
+const assets = require('../../dist/assets.json')
 
 const isOnline = process.env.ONLINE
-console.log('isOnline', isOnline)
+const filterStatic = reg => assets.filter(item => reg.test(item.name)).map(item => item.name).reverse()
 
 module.exports = {
+  styleCreator() {
+    if (isOnline) {
+      return filterStatic(/\.css$/)
+    } else {
+      return []
+    }
+  },
   scriptCreator() {
-    return [`http://localhost:${config.dev.port}/index.js`]
+    if (isOnline) {
+      return filterStatic(/\.js$/)
+    } else {
+      return [`http://localhost:${config.dev.port}/index.js`]
+    }
   }
 }
